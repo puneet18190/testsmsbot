8be078a6d743dba771705ec5aa1cef95dd6c9895
Puneet <puneetgupta18190@gmail.com>
2018-05-08 05:46:18 +0000

    code

--- config/application.rb
+++ config/application.rb
@@ -10,8 +10,6 @@ module Chatbot
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-    config.filestack_rails.api_key = 'kARGphPmj9B3'
-	config.filestack_rails.client_name = 'filestack_client'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
